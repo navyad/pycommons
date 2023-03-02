30b7477a3cb2e8ded1fb8f60b9b46063533cdd51
navyad <navyad.pro@gmail.com>
2023-03-02 12:42:05 +0530

    strf and strp

--- date_times.py
+++ date_times.py
@@ -1,17 +1,29 @@
-from datetime import datetime, timezone, tzinfo
+from datetime import datetime, tzinfo
 
 
 def _datetime_now(tz=None) -> datetime:
     return datetime.now(tz)
 
+
 def now_local() -> datetime:
     return _datetime_now()
+
 
 def now_with_timezone(tz: tzinfo) -> datetime:
     return _datetime_now(tz)
 
+
 def now_utc_datetime() -> datetime:
     return datetime.utcnow()
 
+
 def now_utc_datetime_with_timezone(tz: tzinfo) -> datetime:
     return _datetime_now(tz)
+
+
+def get_datetime_instance(datetime_string: str, date_format: str) -> datetime:
+    return datetime.strptime(datetime_string, date_format)
+
+
+def get_datetime_string(date_time: datetime, date_format: str) -> str:
+    return date_time.strftime(date_format)
